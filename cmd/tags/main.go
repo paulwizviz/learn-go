b7b513490934879b77fcfb58e80c55e07f9e6244
Paul Sitoh <paulwizviz@gmail.com>
2021-11-09 20:35:16 +0000

    Extract tags
    Multiple tags
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- cmd/tags/main.go
+++ cmd/tags/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 type Person struct {
-	FirstName string `json:"firstname"`
-	Surname   string `json:"surname"`
+	FirstName string `json:"firstname" c:"first_name"`
+	Surname   string `json:"surname" c:"surname"`
 }
 
 func ExtractTags(data interface{}) {
@@ -15,12 +17,25 @@ func ExtractTags(data interface{}) {
 	for index := 0; index < elm.NumField(); index++ {
 		fn := elm.Type().Field(index).Name
 		tag := elm.Type().Field(index).Tag
-		fmt.Printf("Field name: %v Tag: %v\n", fn, tag)
+		fmt.Println("Field name: ", fn)
+
+		for _, tElm := range SplitTags(tag) {
+			kv := strings.Split(tElm, ":")
+			fmt.Printf("  tag: %v value: %v\n", tElm, tag.Get(kv[0]))
+		}
 	}
+}
+
+func SplitTags(tag reflect.StructTag) []string {
+	elms := strings.Split(string(tag), " ")
+	return elms
 }
 
 func main() {
 	fmt.Println("====== Extract Person Tag ===========")
 	p := &Person{}
+
+	result, _ := json.Marshal(p)
+	fmt.Println("->", string(result))
 	ExtractTags(p)
 }
