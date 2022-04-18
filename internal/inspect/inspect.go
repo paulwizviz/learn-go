7ae6e1abc7ef1e7e59c773f0676a1ed4f5eaf428
Paul Sitoh <paulwizviz@gmail.com>
2022-04-18 16:34:05 +0100

    Introspection example
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ internal/inspect/inspect.go
@@ -0,0 +1,110 @@
+package inspect
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type FieldElement struct {
+	Type string
+	Kind string
+}
+
+type StructMeta interface {
+	Name() string
+	Fields() []reflect.StructField
+	AppendField(field reflect.StructField)
+}
+
+type structMeta struct {
+	name   string
+	fields []reflect.StructField
+}
+
+func (s *structMeta) Name() string {
+	return s.name
+}
+
+func (s *structMeta) Fields() []reflect.StructField {
+	return s.fields
+}
+
+func (s *structMeta) AppendField(field reflect.StructField) {
+	s.fields = append(s.fields, field)
+}
+
+func NewStructMeta(structName string) StructMeta {
+	return &structMeta{
+		name: structName,
+	}
+}
+
+func ExtractStructMeta(data interface{}) (StructMeta, error) {
+
+	value := reflect.Indirect(reflect.ValueOf(data))
+	if value.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("Not struct")
+	}
+
+	strutName := value.Type().String()
+	sm := NewStructMeta(strutName)
+	for index := 0; index < value.NumField(); index++ {
+		sm.AppendField(value.Type().Field(index))
+	}
+
+	return sm, nil
+}
+
+func ExtractFieldNames(data interface{}) map[string]FieldElement {
+
+	fieldElms := make(map[string]FieldElement)
+
+	elm := reflect.Indirect(reflect.ValueOf(data))
+
+	for index := 0; index < elm.NumField(); index++ {
+		fn := elm.Type().Field(index).Name
+		ft := elm.Type().Field(index).Type
+
+		if ft.Kind() == reflect.Struct {
+			fieldElms[fn] = FieldElement{
+				Type: fmt.Sprintf("%v", ft),
+				Kind: fmt.Sprintf("%v", ft.Kind()),
+			}
+			fd := elm.Type().Field(index)
+			for i := 0; i < fd.Type.NumField(); i++ {
+				sfn := fd.Type.Field(i).Name
+				sft := fd.Type.Field(i).Type
+				fieldElms[sfn] = FieldElement{
+					Type: fmt.Sprintf("%v", sft),
+					Kind: fmt.Sprintf("%v", sft.Kind()),
+				}
+			}
+		} else {
+			fieldElms[fn] = FieldElement{
+				Type: fmt.Sprintf("%v", ft),
+				Kind: fmt.Sprintf("%v", ft.Kind()),
+			}
+		}
+	}
+	return fieldElms
+}
+
+func ExtractTags(data interface{}) {
+	elm := reflect.ValueOf(data).Elem()
+	for index := 0; index < elm.NumField(); index++ {
+		fn := elm.Type().Field(index).Name
+		tag := elm.Type().Field(index).Tag
+		fmt.Println("Field name: ", fn)
+
+		for _, tElm := range SplitTags(tag) {
+			kv := strings.Split(tElm, ":")
+			fmt.Printf("  tag: %v value: %v\n", tElm, tag.Get(kv[0]))
+		}
+	}
+}
+
+func SplitTags(tag reflect.StructTag) []string {
+	elms := strings.Split(string(tag), " ")
+	return elms
+}
---
+++ internal/inspect/inspect_test.go
@@ -0,0 +1,190 @@
+package inspect
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type IPerson interface{}
+
+type Person struct {
+	FirstName string
+	Surname   string
+}
+
+type Address struct {
+	Street  string
+	City    string
+	Country string
+}
+
+type PersonAddress struct {
+	Person
+	Address
+	Landsize int
+}
+
+func fname() {}
+
+// This example demonstrates the TypeOf function in the reflect package
+func Example_typesOf() {
+
+	intValue := 1
+	typ := reflect.TypeOf(intValue)
+	fmt.Printf("Primitive. Type: %T Value: %v Kind: %v\n", typ, typ, typ.Kind())
+
+	intPtr := &intValue
+	typ = reflect.TypeOf(intPtr)
+	fmt.Printf("Pointer. Type: %T Value: %v Kind: %v Element: %v\n", typ, typ, typ.Kind(), typ.Elem())
+
+	type Integer int
+	var iVal Integer = Integer(1)
+	typ = reflect.TypeOf(iVal)
+	fmt.Printf("Alias. Type: %T Value: %v Kind: %v\n", typ, typ, typ.Kind())
+
+	p := Person{
+		FirstName: "firstname",
+	}
+	typ = reflect.TypeOf(p)
+	fmt.Printf("Struct. Type: %T Value: %v Kind: %v\n", typ, typ, typ.Kind())
+
+	pPtr := &Person{}
+	typ = reflect.TypeOf(pPtr)
+	fmt.Printf("Struct pointer. Type: %T Value: %v Kind: %v Element: %v\n", typ, typ, typ.Kind(), typ.Elem())
+
+	var pI IPerson = &Person{}
+	typ = reflect.TypeOf(pI)
+	fmt.Printf("Struct interface. Type: %T Value: %v Kind: %v Element: %v\n", typ, typ, typ.Kind(), typ.Elem())
+
+	typ = reflect.TypeOf(fname)
+	fmt.Printf("Func. Type: %T Value: %v Kind: %v\n", typ, typ, typ.Kind())
+
+	typ = reflect.TypeOf(func() string { return "" })
+	fmt.Printf("Func. Type: %T Value: %v Kind: %v\n", typ, typ, typ.Kind())
+
+	// Output:
+	// Primitive. Type: *reflect.rtype Value: int Kind: int
+	// Pointer. Type: *reflect.rtype Value: *int Kind: ptr Element: int
+	// Alias. Type: *reflect.rtype Value: inspect.Integer Kind: int
+	// Struct. Type: *reflect.rtype Value: inspect.Person Kind: struct
+	// Struct pointer. Type: *reflect.rtype Value: *inspect.Person Kind: ptr Element: inspect.Person
+	// Struct interface. Type: *reflect.rtype Value: *inspect.Person Kind: ptr Element: inspect.Person
+	// Func. Type: *reflect.rtype Value: func() Kind: func
+	// Func. Type: *reflect.rtype Value: func() string Kind: func
+}
+
+// This example demonstrates the use of ValueOf method in the reflect package
+func Example_valueOf() {
+
+	intValue := 1
+	value := reflect.ValueOf(intValue)
+	fmt.Printf("Primitive. Type: %T Value: %v Kind: %v\n", value, value, value.Kind())
+
+	intPtr := &intValue
+	value = reflect.ValueOf(intPtr)
+	fmt.Printf("Primitive. Type: %T Value: %v Kind: %v Element: %v\n", value, "0x...", value.Kind(), value.Elem()) // Value == pointer addree
+
+	p := Person{}
+	value = reflect.ValueOf(p)
+	fmt.Printf("Empty struct. Type: %T Value: %v Kind: %v\n", value, value, value.Kind())
+
+	p1 := Person{
+		FirstName: "firstname",
+	}
+	value = reflect.ValueOf(p1)
+	fmt.Printf("Filled Struct. Type: %T Value: %v Kind: %v\n", value, value, value.Kind())
+
+	ptr := &Person{
+		FirstName: "firstname",
+	}
+	value = reflect.ValueOf(ptr)
+	fmt.Printf("Filled Struct ptr. Type: %T Value: %v Kind: %v Element: %v\n", value, value, value.Kind(), value.Elem())
+
+	value = reflect.ValueOf(fname)
+	fmt.Printf("Func. Type: %T Value: %v Kind: %v\n", value, "0x...", value.Kind()) // value == address of func
+
+	value = reflect.ValueOf(func() string { return "" })
+	fmt.Printf("Func. Type: %T Value: %v Kind: %v\n", value, "0x...", value.Kind())
+
+	// Output:
+	// Primitive. Type: reflect.Value Value: 1 Kind: int
+	// Primitive. Type: reflect.Value Value: 0x... Kind: ptr Element: 1
+	// Empty struct. Type: reflect.Value Value: { } Kind: struct
+	// Filled Struct. Type: reflect.Value Value: {firstname } Kind: struct
+	// Filled Struct ptr. Type: reflect.Value Value: &{firstname } Kind: ptr Element: {firstname }
+	// Func. Type: reflect.Value Value: 0x... Kind: func
+	// Func. Type: reflect.Value Value: 0x... Kind: func
+}
+
+// Reflect indirect demonstrate the extraction of value
+// of either a pointer or non-pointer
+func Example_reflectIndirect() {
+	p := Person{}
+	value := reflect.Indirect(reflect.ValueOf(p))
+	fmt.Printf("Type: %T Value: %v Kind: %v\n", value, value, value.Kind())
+
+	ptr := &Person{}
+	value = reflect.Indirect(reflect.ValueOf(ptr))
+	fmt.Printf("Type: %T Value: %v Kind: %v\n", value, value, value.Kind())
+
+	// Output:
+	// Type: reflect.Value Value: { } Kind: struct
+	// Type: reflect.Value Value: { } Kind: struct
+}
+
+func Example_fields() {
+	pa := PersonAddress{}
+	ptrPA := &pa
+
+	fmt.Println("-- Using typeOf --")
+	typ := reflect.TypeOf(pa)
+	for index := 0; index < typ.NumField(); index++ {
+		fmt.Println(typ.Field(index))
+	}
+
+	fmt.Println("---")
+
+	typ = reflect.TypeOf(ptrPA)
+	for index := 0; index < typ.Elem().NumField(); index++ {
+		fmt.Println(typ.Elem().Field(index))
+	}
+
+	fmt.Println("-- Using valueOf --")
+	value := reflect.Indirect(reflect.ValueOf(pa))
+	for index := 0; index < value.NumField(); index++ {
+		fmt.Println(value.Type().Field(index))
+	}
+
+	fmt.Println("---")
+
+	value = reflect.Indirect(reflect.ValueOf(ptrPA))
+	for index := 0; index < value.NumField(); index++ {
+		fmt.Println(value.Type().Field(index))
+	}
+
+	// Output:
+	// -- Using typeOf --
+	// {Person  inspect.Person  0 [0] true}
+	// {Address  inspect.Address  32 [1] true}
+	// {Landsize  int  80 [2] false}
+	// ---
+	// {Person  inspect.Person  0 [0] true}
+	// {Address  inspect.Address  32 [1] true}
+	// {Landsize  int  80 [2] false}
+	// -- Using valueOf --
+	// {Person  inspect.Person  0 [0] true}
+	// {Address  inspect.Address  32 [1] true}
+	// {Landsize  int  80 [2] false}
+	// ---
+	// {Person  inspect.Person  0 [0] true}
+	// {Address  inspect.Address  32 [1] true}
+	// {Landsize  int  80 [2] false}
+}
+
+func Example_inspectStructs() {
+	p := PersonAddress{}
+	result, err := ExtractStructMeta(p)
+	fmt.Println(result, err)
+
+	// Output:
+}
