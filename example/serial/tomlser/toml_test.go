7e4a3bb50ddd45afd3ee9e6338bf0ea6f33180e9
Paul Sitoh <paulwizviz@gmail.com>
2022-06-03 21:31:15 +0100

    Add toml example
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- example/serial/tomlser/toml_test.go
+++ example/serial/tomlser/toml_test.go
@@ -1,7 +1,8 @@
 package tomlser
 
 import (
-	"log"
+	"fmt"
+	"sort"
 
 	"github.com/spf13/viper"
 )
@@ -14,9 +15,23 @@ func Example_viper() {
 	viper.ReadInConfig()
 
 	keys := viper.AllKeys()
+	sort.Strings(keys)
 	for _, key := range keys {
-		log.Println(key)
+		fmt.Printf("Key: %v Value: %v\n", key, viper.Get(key))
 	}
 
 	// Output:
+	// Key: clients.data Value: [[gamma delta] [1 2]]
+	// Key: clients.hosts Value: [alpha omega]
+	// Key: database.connection_max Value: 5000
+	// Key: database.enabled Value: true
+	// Key: database.ports Value: [8000 8001 8002]
+	// Key: database.server Value: 192.168.1.1
+	// Key: owner.dob Value: 1979-05-27 07:32:00 -0800 -0800
+	// Key: owner.name Value: Tom Preston-Werner
+	// Key: servers.alpha.dc Value: eqdc10
+	// Key: servers.alpha.ip Value: 10.0.0.1
+	// Key: servers.beta.dc Value: eqdc10
+	// Key: servers.beta.ip Value: 10.0.0.2
+	// Key: title Value: TOML Example
 }
